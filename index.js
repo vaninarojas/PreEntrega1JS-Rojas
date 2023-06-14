a21e6e6a6016c12834b3825e75e0c9e29e174da9
vaninarojas <vanina_93@hotmail.com>
2023-06-14 09:43:19 -0300

    Primera Pre entrega Java Script

---
+++ index.js
@@ -0,0 +1,32 @@
+
+let ingresarprodu = prompt ("Ingresar producto: ")
+ while (ingresarprodu != "ESC" ) {
+
+   switch(ingresarprodu){
+case "labial":
+alert("El precio del labial es de 300 pesos") 
+break;
+case "rubor":
+   alert("El precio del rubor es de 500 pesos")
+break;
+case "brochas":
+   alert ("El precio de las brochas es de 1000 pesos")
+break;
+case "gliter":
+   alert("El precio del gliter es de 700 pesos")
+break;
+
+default:
+   alert("No ingresaste ningun producto")
+
+break;} 
+
+ingresarprodu=prompt("Ingresar producto")
+
+}
+
+
+
+    
+ 
+
